86283f675b1935aa663e8bff374bc5a06f8db4cf
Coldcolin <decorcecolin@gmail.com>
2026-05-07 21:09:37 +0100

    updated user info

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,14 +3,15 @@ import { useSelector } from "react-redux";
 export const AuthContext = createContext();
 
 
-const AuthProvider = ({ children }) => {const users = useSelector(state => state.users);
+const AuthProvider = ({ children }) => {
+  // const users = useSelector(state => state.users);
   const [user, setUser] = useState(null);
   // const accounts = user?.accounts.map(account => account);
   const [fromAccount, setFromAccount] = useState(null);
 
   useEffect(() => {
-    console.log(fromAccount);
-  }, [fromAccount]);
+    console.log("context",user);
+  }, [user]);
  
   return <AuthContext.Provider value={{ user, setUser, setFromAccount, fromAccount }}>{children}</AuthContext.Provider>;
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import store from './redux/store.js'
 import AuthProvider from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <Provider store={store}>
       <AuthProvider>
         <App />
       </AuthProvider>
     </Provider>
-  </StrictMode>,
+  // </StrictMode>,
 )
